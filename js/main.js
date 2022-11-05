db77260489c391563aee0573956c59d26b87c9c2
Under4groos <hhit934@gmail.com>
2022-11-05 16:33:29 +0300

    вв

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    main()
+});
+let NAME_APP = "Wallpaper Engine"
+
+
+
+// document.querySelectorAll('[id=name]').forEach(element => {
+
+//     console.log(element.id);
+// });
+for (let elem of document.body.children) {
+    if (elem.id == "name") {
+        console.log(element.id);
+    }
+}
+
+function main() {
+
+}
